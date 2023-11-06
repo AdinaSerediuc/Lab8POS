e4f86f31910d9a38b1fa4a59daf087dbd6b51fc3
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-11-06 22:04:39 +0200

    Add project files.

---
+++ Lab8POS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab8POS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
